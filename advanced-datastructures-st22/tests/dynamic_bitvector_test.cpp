7a245c551df41ad546d692d6f7129974dc9e7777
Tobias Fuchs <info@tobiasfuchs.de>
2022-05-26 21:22:18 +0200

    Add tests for all dynamic bitvector operations (except delete)

--- advanced-datastructures-st22/tests/dynamic_bitvector_test.cpp
+++ advanced-datastructures-st22/tests/dynamic_bitvector_test.cpp
@@ -44,8 +44,6 @@ TEST(ads_test_suite, dynamic_bitvector_set_test) {
     bv.insert(i, i % 3 == 1);
   }
   ASSERT_EQ(bv.size(), 1200);
-  ASSERT_EQ(bv.get_tree_structure(),
-            "(512 171 (256 85 (256 85)(256 86))(256 85 (256 85)(432 144)))");
   for (size_t i = 0; i < 1200; ++i) {
     ASSERT_EQ(bv[i], i % 3 == 1);
   }
@@ -66,8 +64,6 @@ TEST(ads_test_suite, dynamic_bitvector_flip_test) {
     bv.insert(i, i % 3 == 0);
   }
   ASSERT_EQ(bv.size(), 1200);
-  ASSERT_EQ(bv.get_tree_structure(),
-            "(512 171 (256 86 (256 86)(256 85))(256 85 (256 85)(432 144)))");
   for (size_t i = 0; i < 1200; ++i) {
     ASSERT_EQ(bv[i], i % 3 == 0);
   }
@@ -79,5 +75,47 @@ TEST(ads_test_suite, dynamic_bitvector_flip_test) {
     ASSERT_EQ(bv[i], (i % 3 == 0) ^ (i % 5 == 0));
   }
 }
+
+TEST(ads_test_suite, dynamic_bitvector_rank_test) {
+  ads::bv::DynamicBitVector<uint16_t, uint32_t, 8, 16, 32> bv;
+  ASSERT_EQ(bv.size(), 0);
+
+  for (size_t i = 0; i < 1200; ++i) {
+    bv.insert(i, i % 3 == 2);
+  }
+  ASSERT_EQ(bv.size(), 1200);
+  for (size_t i = 0; i < 1200; ++i) {
+    ASSERT_EQ(bv[i], i % 3 == 2);
+    ASSERT_EQ(bv.rank(true, i), i / 3);
+    ASSERT_EQ(bv.rank(false, i), i - i / 3);
+  }
+
+  ASSERT_EQ(bv.rank(true, 1), 0);
+  ASSERT_EQ(bv.rank(true, 60), 20);
+  ASSERT_EQ(bv.rank(true, 600), 200);
+  ASSERT_EQ(bv.rank(false, 1), 1);
+  ASSERT_EQ(bv.rank(false, 60), 40);
+  ASSERT_EQ(bv.rank(false, 600), 400);
+}
+
+TEST(ads_test_suite, dynamic_bitvector_select_test) {
+  ads::bv::DynamicBitVector<uint64_t, uint64_t, 8, 16, 32> bv;
+  ASSERT_EQ(bv.size(), 0);
+
+  for (size_t i = 0; i < 30000; ++i) {
+    bv.insert(i, i % 3 == 1);
+  }
+  ASSERT_EQ(bv.size(), 30000);
+  for (size_t i = 0; i < 30000; i += 3) {
+    ASSERT_EQ(bv[i], false);
+    ASSERT_EQ(bv[i + 1], true);
+    ASSERT_EQ(bv[i + 2], false);
+    ASSERT_EQ(bv.select(false, i - i / 3 + 1), i);
+    ASSERT_EQ(bv.select(true, i / 3 + 1), i + 1);
+    ASSERT_EQ(bv.select(false, i - i / 3 + 2), i + 2);
+  }
+}
+
+TEST(ads_test_suite, dynamic_bitvector_delete_test) {}
 
 }  // namespace ads_test
