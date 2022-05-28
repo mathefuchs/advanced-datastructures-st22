947f6bfa39ed25fac6bb46f3426784eedd75ab4f
Tobias Fuchs <info@tobiasfuchs.de>
2022-05-28 19:56:15 +0200

    Add bitvector benchmark examples

---
+++ examples/run_long.sh
@@ -0,0 +1,2 @@
+set -x
+../build/advanced-datastructures-st22/ads_programm_a bv long.txt long_output.txt
---
+++ examples/run_simple.sh
@@ -0,0 +1,2 @@
+set -x
+../build/advanced-datastructures-st22/ads_programm_a bv simple.txt simple_output.txt
