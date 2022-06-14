029600255d821e19728b0681457cfce69afb834c
Tobias Fuchs <info@tobiasfuchs.de>
2022-06-14 23:43:39 +0200

    Add bitvector evaluation

---
+++ bv_benchmark.sh
@@ -0,0 +1,5 @@
+build/advanced-datastructures-st22/ads_programm_a bv examples/bv_benchmark.txt /dev/null >> examples/bv_results.txt
+build/advanced-datastructures-st22/ads_programm_a bv examples/bv_benchmark.txt /dev/null >> examples/bv_results.txt
+build/advanced-datastructures-st22/ads_programm_a bv examples/bv_benchmark.txt /dev/null >> examples/bv_results.txt
+build/advanced-datastructures-st22/ads_programm_a bv examples/bv_benchmark.txt /dev/null >> examples/bv_results.txt
+build/advanced-datastructures-st22/ads_programm_a bv examples/bv_benchmark.txt /dev/null >> examples/bv_results.txt
