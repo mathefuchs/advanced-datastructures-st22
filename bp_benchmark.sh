181a049a099f44afe33303b93be2c7de272dc4c8
Tobias Fuchs <info@tobiasfuchs.de>
2022-06-14 18:07:15 +0200

    Add BP benchmark results

---
+++ bp_benchmark.sh
@@ -0,0 +1,5 @@
+build/advanced-datastructures-st22/ads_programm_a bp examples/bp_benchmark.txt /dev/null >> examples/bp_results.txt
+build/advanced-datastructures-st22/ads_programm_a bp examples/bp_benchmark.txt /dev/null >> examples/bp_results.txt
+build/advanced-datastructures-st22/ads_programm_a bp examples/bp_benchmark.txt /dev/null >> examples/bp_results.txt
+build/advanced-datastructures-st22/ads_programm_a bp examples/bp_benchmark.txt /dev/null >> examples/bp_results.txt
+build/advanced-datastructures-st22/ads_programm_a bp examples/bp_benchmark.txt /dev/null >> examples/bp_results.txt
