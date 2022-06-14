63e60fd90676e39bc51a40ba2b5005b9bbda93fb
Tobias Fuchs <info@tobiasfuchs.de>
2022-06-14 16:04:47 +0200

    Add all operations to reliability test

--- advanced-datastructures-st22/tests/reliability_test.cpp
+++ advanced-datastructures-st22/tests/reliability_test.cpp
@@ -12,7 +12,8 @@ namespace ads_reliability_test {
 TEST(ads_reliability_test_suite, dynamic_bp_tree_test) {
   srand(42);
   std::ofstream query_file("examples/bp_benchmark.txt", std::ios::trunc);
-  std::ofstream output_file("examples/bp_benchmark_output.txt", std::ios::trunc);
+  std::ofstream output_file("examples/bp_benchmark_output.txt",
+                            std::ios::trunc);
   ASSERT_TRUE(query_file.is_open());
   ASSERT_TRUE(output_file.is_open());
 
@@ -27,8 +28,23 @@ TEST(ads_reliability_test_suite, dynamic_bp_tree_test) {
       const size_t child = num_children == 0 ? 1 : (rand() % num_children) + 1;
       const size_t take_children =
           child >= num_children ? 0 : rand() % (num_children - child);
-      query_file << "insertchild 0 " << child << " " << take_children
-                 << "\n";
+
+      if (num_children != 0) {
+        const size_t child_v = actual.i_th_child(0, child);
+        const size_t num_child_child = actual.num_children(child_v);
+        if (num_child_child > 0) {
+          query_file << "parent " << (child_v + 1) << "\n";
+          output_file << child_v << "\n";
+
+          query_file << "child " << child_v << " " << num_child_child << "\n";
+          output_file << actual.i_th_child(child_v, num_child_child) << "\n";
+        } else {
+          query_file << "parent " << actual.i_th_child(0, child) << "\n";
+          output_file << "0\n";
+        }
+      }
+
+      query_file << "insertchild 0 " << child << " " << take_children << "\n";
       actual.insert_node(0, child, take_children);
     }
     {
@@ -38,6 +54,10 @@ TEST(ads_reliability_test_suite, dynamic_bp_tree_test) {
       query_file << "deletenode " << delete_node << "\n";
       actual.delete_node(delete_node);
     }
+
+    query_file << "subtree_size 0\n";
+    output_file << (((i + 1) * 2) + 1) << "\n";
+
     // Check DFS traversal string creation
     if (i % (n / 100) == 0) {
       std::ostringstream oss;
